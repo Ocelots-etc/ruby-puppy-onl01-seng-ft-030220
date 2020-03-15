36e62843e88dddc94f2a21ea90aaae27a15ea9a0
Cat Gomez <catherinedgomez@gmail.com>
2020-03-15 13:05:38 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,1 +1,44 @@
-# Add your code here
+class Dog
+
+  attr_accessor :name, :save
+
+@@all = []
+# @@name = []
+
+def initialize(name)
+  @name = name
+  # @@all << self
+  save
+end
+
+def self.all
+  @@all.each do |dog|
+    puts dog.name
+  end
+end
+
+def self.clear_all
+  @@all.clear
+end
+
+def self.print_all
+  @@all.each do |dog|
+    puts dog.name
+  end
+end
+
+def self.save
+    save = {}
+      @@save.each do |name|
+      if !save.has_key?(name)
+        save[name] = 1
+      else
+        save[name] += 1
+      end
+      name = @@all
+    end
+    save
+    @@all << self
+  end
+
+end
